49fc0897c556608ba7505101b31d833ff0032a08
Kelly <kellyjchen.kc@gmail.com>
2019-07-31 03:12:44 -0400

    Add circle to satellite coverage.

--- SatelliteCoverageMap/CoverageMap.xaml.cs
+++ SatelliteCoverageMap/CoverageMap.xaml.cs
@@ -7,23 +7,31 @@ namespace SatelliteCoverageMap
     {
         public CoverageMap()
         {
-            Map map = new Map(
-                MapSpan.FromCenterAndRadius(
-                        new Position(35, -100), Distance.FromMiles(5000)))
+            var map = new CustomMap()
             {
+                MapType = MapType.Hybrid,
                 IsShowingUser = true,
                 HeightRequest = 100,
                 WidthRequest = 960,
                 VerticalOptions = LayoutOptions.FillAndExpand
             };
+
+            DrawContours(map, new Position(35, -100), 2000);
+
             var stack = new StackLayout { Spacing = 0 };
             stack.Children.Add(map);
             Content = stack;
-
         }
 
-        public void DrawContours()
+        public void DrawContours(CustomMap map, Position position, double diameter)
         {
+
+            map.Circle = new CustomCircle
+            {
+                Position = position,
+                Radius = diameter / 2
+            };
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(position, Distance.FromMiles(1.0)));
         }
     }
 }
---
+++ SatelliteCoverageMap/CustomMap.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Xamarin.Forms.Maps;
+
+namespace SatelliteCoverageMap
+{
+    public class CustomMap : Map
+    {
+        public CustomCircle Circle { get; set; }
+    }
+}
